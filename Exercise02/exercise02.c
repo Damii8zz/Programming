341d13064a99cba871f7976e152503074e0c4378
Damiizzzzz <damia.fillatf@gmail.com>
2020-10-28 10:25:41 +0100

    Programming

--- Exercise02/exercise02.c
+++ Exercise02/exercise02.c
@@ -4,18 +4,16 @@
 
 int main()
 {
-	int number01 = 0
-	int number02 = 0
+	int number01 = 0;
+	int number02 = 0;
 
 	printf("Introduce a number:");
+	scanf_s("%i\n" &number01);
 
-	scanf("%i\n" number01);
 	printf("Introduce another number:");
-	getchar();
-
-	scanf("%i\n" number02);
-	printf("The sum of this two numbers is: %i\n" number01 + number02);
-	getchar();
-
+	scanf_s("%i\n" &number02);
+	
+	printf("The sum of this two numbers is: %i\n", number01 + number02);
+	
 	return 0;
 }
